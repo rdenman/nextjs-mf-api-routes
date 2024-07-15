040b7493e0828152ade02fa6553df8e4954ec3d1
Rob Denman <rob.denman@datasite.com>
2024-07-15 16:50:09 -0500

    recreate issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
+import NextFederationPlugin from "@module-federation/nextjs-mf";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  webpack(config) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "nextjs-mf-api-routes",
+        filename: "static/chunks/remoteEntry.js",
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
